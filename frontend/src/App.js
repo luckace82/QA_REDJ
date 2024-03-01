00d60e0defad0168e89d282c538dc7b1e3be9beb
luckace82 <lakeshshrestha82@gmail.com>
2024-03-01 23:39:46 +0545

    restart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,31 @@ class App extends Component {
   }
 
   componentDidMount() {
-    
     fetch('http://127.0.0.1:8000/Questions/')
       .then(response => response.json())
-      
-      
-      }
-      ;
+      .then(data => {
+        // Update the state with the fetched questions
+        this.setState({ questions: data });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }
 
+  render() {
+    const { questions } = this.state;
 
-
-  
-  
+    return (
+      <div>
+        <h1>Questions</h1>
+        <ul>
+          {questions.map(question => (
+            <li key={question.id}>{question.question_txt}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
